f7b7cc49d698a584205f7b69d9e3a0bc4dba00b0
gaforlauren127 <laurenforero0@gmail.com>
2026-05-12 23:15:05 -0500

    Create Mi_Perfil_Digital.py

---
+++ Mi_Perfil_Digital.py
@@ -0,0 +1,28 @@
+#uso de los tipos de datos en python
+# 1. Datos basicos (str, int, bool, float)
+
+nombre = "Lauren Michell Forero Gómez"
+edad = 13
+estatura = 1.60
+es_estudiante = True
+
+# 2. Redes_Sociales = (tuple)
+
+Redes_sociales = ("gaforlauren127", "
+lau_mxchxlforero12")
+
+# 3. Playlist de cantantes favoritos = (list en un dict)
+
+Playlist = [{"titulo": "La suerte está echada", "artista": "Diomedes Díaz", "duracion": "5:05"},
+{"titulo": "Ayer y hoy", "artista": "combinación vallenata", "duracion": "5:03"},
+{"titulo": "Ella es mi todo", "artista": "Kalet Morales", "duracion": "4:41"}]
+
+print("presentacion personal")
+print("Mi nombre es:", nombre)
+print("Mi edad es:", edad)
+print("Mi estatura es:", estatura)
+print("¿estoy activo en el colegio?", es_estudiante)
+print("Mis redes sociales son:", Redes_sociales)
+print("Mi playlist favorita:") 
+print(f"{cancion["titulo"]} - {cancion["artista"]})({cancion["duracion"]})min")
+print ("----------------------------------")
